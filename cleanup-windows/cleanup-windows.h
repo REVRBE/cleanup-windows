4ccff3b82f29416dd54daef78389f388731e86b7
REVRBE <129604052+REVRBE@users.noreply.github.com>
2023-10-21 00:41:39 +0200

    Update (appcompat installs)

--- cleanup-windows/cleanup-windows.h
+++ cleanup-windows/cleanup-windows.h
@@ -13,29 +13,41 @@ namespace internals {
 	namespace file_utils {
 		std::wstring get_user_profile_directory();
 
-		void delete_files_in_folder(const std::wstring& folder_path);
+		void delete_files_in_folder_with_read_only(const std::wstring& folder_path);
+
+		void delete_files_in_folder(const std::wstring& folderPath);
 	}
 
 	class cleanup_windows {
 	public:
 		static void start_cleanup() {
+			if (!internals::process_utils::enable_privilege(SE_DEBUG_NAME)) {
+				std::cerr << "[-] Failed to enable debug privilege." << std::endl;
+			}
+
 			if (!clean_icon_and_thumb_cache()) {
+				std::cerr << "[-] Failed to clean icon and thumbcache." << std::endl;
+			}
+			if (!clean_appcompat_installs()) {
 				std::cerr << "[-] Failed to clean icon and thumbcache." << std::endl;
 			}
 		}
 
 	private:
 		static bool clean_icon_and_thumb_cache() {
-			if (!internals::process_utils::enable_privilege(SE_DEBUG_NAME)) {
-				std::cerr << "[-] Failed to enable debug privilege." << std::endl;
-			}
-
 			std::wstring explorer_path = internals::file_utils::get_user_profile_directory();
 			explorer_path += L"\\AppData\\Local\\Microsoft\\Windows\\Explorer\\";
 
 			internals::process_utils::restart_explorer();
 
-			internals::file_utils::delete_files_in_folder(explorer_path);
+			internals::file_utils::delete_files_in_folder_with_read_only(explorer_path);
+			return true;
+		}
+
+		static bool clean_appcompat_installs() {
+			std::wstring installs_path = L"C:\\Windows\\appcompat\\Programs\\Install";
+
+			internals::file_utils::delete_files_in_folder(installs_path);
 			return true;
 		}
 	};
